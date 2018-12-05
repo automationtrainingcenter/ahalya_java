4d663ec7309d83a18a97e1ed16e26cfce2424eea
automationtrainingcenter <atcsurya@gmail.com>
2018-12-05 13:36:23 +0530

    comments and variables

---
+++ src/basics/CommentsDemo.java
@@ -0,0 +1,16 @@
+package basics;
+// comments are lines of statements which will not execute
+
+/*
+ * this is multiline
+ * comment
+ */
+
+/**
+ * This is documentation comment
+ * This class explains comments in Java
+ */
+
+public class CommentsDemo {
+
+}

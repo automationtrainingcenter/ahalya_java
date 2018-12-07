3841dab8f5e4ff97eb8d37fea6a87ca3c834f227
Ahalya14 <emailme.ahalya548@gmail.com>
2018-12-07 06:47:40 +0530

    modified AhalyaVariablesAssignment

--- src/Ahalyaassignments/Variablesassignment.java
+++ src/Ahalyaassignments/Variablesassignment.java
@@ -20,27 +20,25 @@ float salary;
 long adhar;
 public void display()
 {
-System.out.println("empname:");
-System.out.println("number:");
-System.out.println("companyname:");
+System.out.println("empname:"+empname);
+System.out.println("companyname:"+companyname);
 }
 public static void main(String[] args)
 {
 	Variablesassignment e1= new Variablesassignment();
 	e1.empname="pinky";
-	e1.number=9848023238;
 	e1.companyname="oracle";
+	e1.display();
 	Variablesassignment e2= new Variablesassignment();
 	e2.empname="Dimple";
-	e2.number=9848032328;
 	e2.companyname="oracle";
+	e2.display();
 	Variablesassignment e3= new Variablesassignment();
 	e3.empname="Tommy";
-	e3.number=9848043438;
 	e3.companyname="oracle";
-    e1.display();
-    e2.display();
-    e3.display();
+	 e3.display();
+   
+   
 }
     
 }

d15165ec7a7008991db6bf3f1f6fdc0dca928c8e
automationtrainingcenter <atcsurya@gmail.com>
2018-12-10 12:15:04 +0530

    ahalaya assignment modification

--- src/Ahalyaassignments/Variablesassignment.java
+++ src/Ahalyaassignments/Variablesassignment.java
@@ -1,4 +1,5 @@
 package Ahalyaassignments;
+
 /*
  * write a program to create employee details like name, id ,
  * adhar number,
@@ -12,33 +13,32 @@ package Ahalyaassignments;
  *
  */
 public class Variablesassignment {
-String empname,address;
-long number;
-static String companyname;
-int empid;
-float salary;
-long adhar;
-public void display()
-{
-System.out.println("empname:"+empname);
-System.out.println("companyname:"+companyname);
-}
-public static void main(String[] args)
-{
-	Variablesassignment e1= new Variablesassignment();
-	e1.empname="pinky";
-	e1.companyname="oracle";
-	e1.display();
-	Variablesassignment e2= new Variablesassignment();
-	e2.empname="Dimple";
-	e2.companyname="oracle";
-	e2.display();
-	Variablesassignment e3= new Variablesassignment();
-	e3.empname="Tommy";
-	e3.companyname="oracle";
-	 e3.display();
-   
-   
-}
-    
+	String empname, address;
+	long number;
+	static String companyname;
+	int empid;
+	float salary;
+	long adhar;
+
+	public void display() {
+		System.out.println("empname:" + empname);
+		System.out.println("companyname:" + companyname);
+	}
+
+	public static void main(String[] args) {
+		Variablesassignment e1 = new Variablesassignment();
+		e1.empname = "pinky";
+		e1.companyname = "oracle";
+		e1.display();
+		Variablesassignment e2 = new Variablesassignment();
+		e2.empname = "Dimple";
+		e2.companyname = "oracle";
+		e2.display();
+		Variablesassignment e3 = new Variablesassignment();
+		e3.empname = "Tommy";
+		e3.companyname = "oracle";
+		e3.display();
+
+	}
+
 }

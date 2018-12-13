62de7307c932cc05431291873294122ea4b5188a
Ahalya14 <emailme.ahalya548@gmail.com>
2018-12-13 08:38:20 +0530

    added vowelsprogram

--- src/Ahalyaassignments/Assignment1.java
+++ src/Ahalyaassignments/Assignment1.java
@@ -1,5 +1,7 @@
 package Ahalyaassignments;
 
+import java.util.Scanner;
+
 public class Assignment1 {
 	// write a method to find out given character is vowel or not using if else statement
 	
@@ -19,6 +21,24 @@ public class Assignment1 {
 	// ex: This is a sample string after conversion THIS is A sample STRING
 	
 	//create a main method and call all the above methods
+	
+	public static void main(String[] args) {
+		int i=0;
+		char ch;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter a character");
+		ch=s.next().charAt(0);
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'|| ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'|| ch=='U')
+			System.out.println("Given character is vowel");
+		else
+			System.out.println("given character is consonant");
+		
+		
+		
+		
+			
+	}
+	
 	
 	
 	
